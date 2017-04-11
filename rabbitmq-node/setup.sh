c1bbbcdb904da5b7bc5ba8a5000d77ff1dd3b486
Sebastian Andreasson <sebastian.andreasson93@gmail.com>
2017-04-11 13:51:55 +0200

    some cleanup in setup file

--- rabbitmq-node/setup.sh
+++ rabbitmq-node/setup.sh
@@ -3,10 +3,9 @@
 setup_erlang_cookie() {
 	if [ "${RABBITMQ_ERLANG_COOKIE:-}" ]; then
 		cookieFile='/var/lib/rabbitmq/.erlang.cookie'
-		if [ -e "$cookieFile" ]; then
-			if [ "$(cat "$cookieFile" 2>/dev/null)" != "$RABBITMQ_ERLANG_COOKIE" ]; then
-				echo >&2 "warning: $cookieFile contents do not match RABBITMQ_ERLANG_COOKIE"
-			fi
+
+		if [ -e "$cookieFile" ] && [ "$(cat "$cookieFile" 2>/dev/null)" != "$RABBITMQ_ERLANG_COOKIE" ]; then
+			echo >&2 "warning: $cookieFile contents do not match RABBITMQ_ERLANG_COOKIE"
 		else
 			echo "$RABBITMQ_ERLANG_COOKIE" > "$cookieFile"
 			chmod 600 "$cookieFile"
@@ -17,10 +16,13 @@ setup_erlang_cookie() {
 setup_default_user() {
 	if [ "${RABBITMQ_DEFAULT_USER:-}" ] && [ "${RABBITMQ_DEFAULT_PASSWORD:-}" ]; then
 		echo "Removing 'guest' user and adding ${RABBITMQ_DEFAULT_USER}"
-		rabbitmqctl delete_user guest
-		rabbitmqctl add_user $RABBITMQ_DEFAULT_USER $RABBITMQ_DEFAULT_PASSWORD
-		rabbitmqctl set_user_tags $RABBITMQ_DEFAULT_USER administrator
-		rabbitmqctl set_permissions -p / $RABBITMQ_DEFAULT_USER ".*" ".*" ".*"
+
+		rabbitmqctl delete_user guest &&
+		rabbitmqctl add_user $RABBITMQ_DEFAULT_USER $RABBITMQ_DEFAULT_PASSWORD &&
+		rabbitmqctl set_user_tags $RABBITMQ_DEFAULT_USER administrator &&
+		rabbitmqctl set_permissions -p / $RABBITMQ_DEFAULT_USER ".*" ".*" ".*" &&
+
+		echo "added user ${RABBITMQ_DEFAULT_USER}"
 	fi
 }
 
@@ -28,7 +30,6 @@ setup_cluster() {
 	if [ "${CLUSTER_WITH:-}" ]; then
 		echo >&2 "SETUP CLUSTER WITH $CLUSTER_WITH"
 
-		# join the cluster
 		rabbitmqctl stop_app &&
 		rabbitmqctl join_cluster rabbit\@$CLUSTER_WITH &&
 		rabbitmqctl start_app
